627491c601d8aa60aec44475d79e19d403978eae
Hikmat <mac@MacBook-Pro.local>
2024-10-22 10:37:23 +0800

    Tugas_1

--- tugas_1.py
+++ tugas_1.py
@@ -1,3 +1,5 @@
+import PySimpleGUI as sg
+sg.theme("DarkGreen4")
 import tkinter as tk
 from tkinter import Label
 
@@ -8,10 +10,10 @@ root.geometry("400x200")
 root.configure(bg='#5A6473')
 
 # Menampilkan label informasi dengan alignment kiri
-Label(root, text="NPM     : 2310010529", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=20)
-Label(root, text="Nama    : MUHAMMAD HIKMATUULAH", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=50)
-Label(root, text="Kelas   : 5A NON REG BANJARMASIN", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=80)
-Label(root, text="Mata kuliah  : PEMROGRAM VISUAL 3", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=110)
+Label(root, text="NPM              : 2310010529", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=20)
+Label(root, text="Nama             : MUHAMMAD HIKMATUULAH", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=50)
+Label(root, text="Kelas            : 5A NON REG BANJARMASIN", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=80)
+Label(root, text="Mata kuliah      : PEMROGRAM VISUAL 3", font=("Arial", 12), bg='#5A6473', fg='white', anchor='w').place(x=20, y=110)
 
 # Menjalankan loop jendela
 root.mainloop()
